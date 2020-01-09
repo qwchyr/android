da95f8edf85c36337589e75627662e20437a4ee9
chyr <1553982721@qq.com>
2020-01-10 01:39:24 +0800

    作业提交

---
+++ shop/app/src/androidTest/java/cn/mmvtc/shop/ExampleInstrumentedTest.java
@@ -0,0 +1,20 @@
+package cn.mmvtc.shop;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() {
+        Context appContext = InstrumentationRegistry.getTargetContext();
+        assertEquals("cn.mmvtc.shop", appContext.getPackageName());
+    }
+}
---
+++ shop/app/src/main/java/cn/mmvtc/shop/bean/ShoppingBean.java
@@ -0,0 +1,41 @@
+package cn.mmvtc.shop.bean;
+
+public class ShoppingBean {
+    private String id;
+    private String ShoppingTitle;
+    private String shoppingPrice;
+    private  int img;
+
+    public int getImg() {
+        return img;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setImg(int img) {
+        this.img = img;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getShoppingPrice() {
+        return shoppingPrice;
+    }
+
+    public void setShoppingPrice(String shoppingPrice) {
+        this.shoppingPrice = shoppingPrice;
+    }
+
+    public String getShoppingTitle() {
+        return ShoppingTitle;
+    }
+
+    public void setShoppingTitle(String shoppingTitle) {
+        ShoppingTitle = shoppingTitle;
+    }
+
+}
